2ab675ead0511943b5a3f00c9f5f25ec3ed8fe95
Mike <mikeknueppel@hotmail.com>
2013-08-03 10:36:07 -0700

    Add mongo_mapper gem, bson_ext gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MatchPoint::Application.config.secret_key_base = '6a19ba1d3933f48edd0d1876788cbf33796bb3f59b7b66a644907d72b1b00fc4b889fa88594ab8ea7409582a1f1f92bd163b455ff5a01210756446a4f1a1b8c0'
