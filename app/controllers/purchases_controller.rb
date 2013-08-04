c1021fbf933580d23e09815daa282c3254d5f4c7
Mike <mikeknueppel@hotmail.com>
2013-08-03 19:15:53 -0700

    Add select boxes for credit card expiration date

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,14 @@ class PurchasesController < ApplicationController
   def new
   	@purchase = Purchase.new
   	@packages = Package.all
+  	@months = ['01','02','03','04','05','06','07','08','09','10',
+  						 '11','12']
+  	year = Time.now.year
+  	@years = []
+  	20.times do
+  		@years.push year
+  		year += 1
+  	end
   end
 
   def create
