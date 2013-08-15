d9c42f1eafe3e3b8c94a6883f1996e0d2feacba1
Mike <mikeknueppel@hotmail.com>
2013-08-15 12:36:12 -0700

    Add fields to Instructor model -add hourly_rate -add years_of_experience

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,7 @@
 class Instructor < User
 	attr_accessor :password, :password_confirmation
 	before_save :encrypt_password
+
+	field :hourly_rate, type: Integer
+	field :years_of_experience, type: Integer
 end
