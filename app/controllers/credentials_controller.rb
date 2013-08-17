dab0171b66d90e790aaad9df1291314c1298c8a7
Mike <mikeknueppel@hotmail.com>
2013-08-16 20:46:59 -0700

    Actually delete Credentials controller this time

--- app/controllers/credentials_controller.rb
+++
@@ -1,8 +0,0 @@
-class CredentialsController < ApplicationController
-  #create action executes when signup form is submitted
-  #note: put create action in Usrs controller
-
-
-  #move to Users controller
-
-end
