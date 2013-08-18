485c5cd485cf9dc906ecbdbbaf0903942643bebe
Mike <mikeknueppel@hotmail.com>
2013-08-18 15:38:06 -0700

    Add one to one association between Order and Instructor

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,7 @@
 class Instructor < User
 	attr_accessor :password, :password_confirmation
 	before_save :encrypt_password
+	belongs_to :order
 
 	field :hourly_rate, type: Integer
 	field :years_of_experience, type: Integer
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order
   include Mongoid::Document
   belongs_to :user
   has_many :lessons
+  has_one :instructor
 
   field :price, type: Integer
   field :quantity, type: Integer
