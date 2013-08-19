2bf8422773682e36273e87888d9416e37d6e9aa1
Mike <mikeknueppel@hotmail.com>
2013-08-18 22:02:12 -0700

    Update lesson status -Send ajax put request to update lesson status when button is clicked in the Instructor dashboard

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,5 +3,37 @@ if it was successful, change the color of the button to
 */
 
 $('.small.button.success').click(function() {
-	console.log($(this).data('id'));
+	console.log("button clicked");
+	var button_clicked = $(this);
+	var lesson_to_update_id = $(this).data('id');
+	var updated_status = '';
+	if ($(this).data('status') == 'Available') {
+		updated_status = 'Used';
+	}
+	else {
+		updated_status = 'Available';
+	}
+
+	$.ajax({
+		url : '/users/'+lesson_to_update_id, 
+		type: 'PUT',
+		data: { 'status' : updated_status}, 
+		success : function(res) {
+
+	if (updated_status == 'Used') {
+		console.log('make it red');
+		button_clicked.removeClass('success');
+		button_clicked.addClass('alert');
+		console.log(button_clicked.attr('class'));
+	}
+	else {
+		console.log('make it green');
+		button_clicked.removeClass('alert');
+		button_clicked.addClass('success');
+		console.log(button_clicked.attr('class'));
+	}
+
+			
+		}//end success callback
+	});//end ajax function
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  respond_to :json
 
   def index
     @customers = Customer.all
@@ -46,21 +47,28 @@ class UsersController < ApplicationController
   end
 
   def update
-  	@user = User.find_by_code(params[:user][:code])
-  	if @user and @user.expires_at > Time.now
-  		@user.update_attributes(params[:user])
-  		@user.code = nil
-  		@user.expires_at = nil
-  		if @user.save!
-  			#account was updated. Login the user
-  			session[:user_id] = @user.id
-  			redirect_to dashboard_path(@user.id)
-  		else
-  			redirect_to dashboard_path(@user.id)
-  		end
-  	else
-  		render "dashboard/index"
-  	end
+    # puts "update*******"
+    # puts params[:id]
+    lesson_to_update = Lesson.find params[:id]
+    lesson_to_update.status = params[:status]
+    lesson_to_update.save
+    render nothing: true
+
+  	# @user = User.find_by_code(params[:user][:code])
+  	# if @user and @user.expires_at > Time.now
+  	# 	@user.update_attributes(params[:user])
+  	# 	@user.code = nil
+  	# 	@user.expires_at = nil
+  	# 	if @user.save!
+  	# 		#account was updated. Login the user
+  	# 		session[:user_id] = @user.id
+  	# 		redirect_to dashboard_path(@user.id)
+  	# 	else
+  	# 		redirect_to dashboard_path(@user.id)
+  	# 	end
+  	# else
+  	# 	render "dashboard/index"
+  	# end
   end
 
   private
