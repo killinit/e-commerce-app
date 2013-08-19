47bbb151985d919f1bae0db2213dc03347a20691
Mike <mikeknueppel@hotmail.com>
2013-08-19 15:16:50 -0700

    Toggle button is working now in partial template _schedule.html.erb

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,3 @@
-/*when green button is clicked, send ajax put request to change the lesson status
-if it was successful, change the color of the button to
-*/
-
 $('.small.button').click(function() {
 	var button_clicked = $(this);
 	var lesson_to_update_id = $(this).data('id');
@@ -16,21 +12,23 @@ $('.small.button').click(function() {
 	$.ajax({
 		url : '/users/'+lesson_to_update_id, 
 		type: 'PUT',
-		data: { 'status' : updated_status}, 
+		data: {'status' : updated_status}, 
 		success : function(res) {
 			if (button_clicked.data('status') == 'Available') {
 				console.log('make it red');
 				button_clicked.removeClass('success');
 				button_clicked.addClass('alert');
 				console.log(button_clicked.attr('class'));
-				button_clicked.data('status') == 'Used';
+				button_clicked.data('status','Used');
+				button_clicked.text('Used');
 			}
 			else {
 				console.log('make it green');
 				button_clicked.removeClass('alert');
 				button_clicked.addClass('success');
 				console.log(button_clicked.attr('class'));
-				button_clicked.data('status') == 'Available';
+				button_clicked.data('status','Available');
+				button_clicked.text('Available');
 			}
 		}//end success callback
 	});//end ajax function
